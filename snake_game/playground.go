b329efeda5390532ff8a22eb1afc6560a4bc8226
aljaz.stajnko <aljaz.stajnko@margento.com>
2022-01-11 15:08:52 +0100

    border added, random display of food added

--- snake_game/playground.go
+++ snake_game/playground.go
@@ -1,16 +1,31 @@
 package snake_game
 
-import (
-	"fmt"
-)
+import ( 
 
-func PrintCoordinatestest() {
-   // test := newSnake("olala")
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/imdraw"
+	"math/rand"
 
-	tmp := new(coordinates)
-	tmp.setCoordinates(3,44)
 
-	cordX,cordY := tmp.getCoordinates()
+	"image/color"
+	)
 
-	fmt.Println(cordX,cordY)
+type border struct {
+	rect  pixel.Rect
+	color color.Color
+	position coordinates
 }
+
+func (f *border) draw(imd *imdraw.IMDraw) {
+	imd.Color = f.color
+	imd.Push(f.rect.Min, f.rect.Max)
+	imd.Rectangle(2)
+}
+
+// return the coordinates of the food on display
+func (f *border) foodPosition() (int, int) {
+	return f.position.x, f.position.y
+}
+
+
+
